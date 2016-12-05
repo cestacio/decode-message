1eadd99f303b8f2efa5f5f5dd798c8b0370cc5f7
Charmaine <charmaine.estacio@gmail.com>
2016-12-04 16:34:47 -0800

    Update foodtruckdata.py

--- src/foodtruck-project/foodtruckdata.py
+++ src/foodtruck-project/foodtruckdata.py
@@ -50,12 +50,29 @@ def clean_data(json_obj):
 		clean_data[truck_name] = truck_cuisine, truck_days, truck_location
 	return clean_data 
 
-def search_by_day(cleaned_data, username, user_day_pref=1):
+def search_by_day(cleaned_data, username, user_day_pref):
 	""" loop through data and searches by day """
+	truck_days = None
+	
+	while truck_days == None: 
+		if user_day_pref == 1:
+			truck_days = ['mo-su', 'mo-fri', 'm', 'mo-sat']
+		elif user_day_pref == 2:
+			truck_days = ['mo-su', 'mo-fri', 'tu', 'mo-sat']
+		elif user_day_pref == 3:
+			truck_days = ['mo-su', 'mo-fri', 'w', 'mo-sat']
+		elif user_day_pref == 4:
+			truck_days = ['mo-su', 'mo-fri', 'th', 'mo-sat']
+		elif user_day_pref == 5:
+			truck_days = ['mo-su', 'mo-fri', 'f', 'mo-sat']
+		elif user_day_pref == 6:
+			truck_days = ['mo-su', 'sat', 'mo-sat']
+		elif user_day_pref == 7:
+			truck_days = ['mo-su','sun']
+		else:
+			print "That's not a valid choice. Try again."
+			user_day_pref = int(raw_input("Select your choice between 1-7. "))
 
-	if user_day_pref == 1:
-		truck_days = ['mo-su', 'mo-fri', 'm', 'mo-sat']
-	# FINISH THIS! 
 
 	display_recommendations(username)
 	# this iterates over every single truck in cleaned data 
@@ -73,8 +90,17 @@ def search_by_cuisine(cleaned_data, user_food_pref, username):
 
 	for truck,info in cleaned_data.items():
 		if info[0] and user_food_pref in info[0]:			
-			print truck, "is at ", info[2]
+			print truck, "is at ", info[2], "\n"
 
+def search_by_truck(cleaned_data, username, user_truck_pref):
+
+	display_recommendations(username)
+	
+	for truck,info in cleaned_data.items():
+		if info[2] and user_truck_pref in truck: 	
+			print truck, "is at", info[2], "\n"
+
+		
 def main():
 	# this is loading data
 	response = call_sfdataapi(FOODTRUCK_ENDPOINT)
@@ -84,28 +110,27 @@ def main():
 	# this is where we prompt user for preferences
 	
 	username = prompt_user_for_username()
-	# while (True):
-	# TODO: write search by truck function, test while loop
-	display_menu()
-	user_menu_choice = prompt_user_menu_choice()
 
-	if user_menu_choice == "A":
-		user_food_pref = prompt_user_for_cuisine()
-		search_by_cuisine(cleaned_data, user_food_pref, username)
+	while(True):
 
-	if user_menu_choice == "B":
+		display_menu()
+		user_menu_choice = prompt_user_menu_choice()
 
-		print day_choices
-		user_day_pref = int(raw_input("Select your choice between 1-7. "))
-		search_by_day(cleaned_data, username, user_day_pref)
+		if user_menu_choice == "A":
+			user_food_pref = prompt_user_for_cuisine()
+			search_by_cuisine(cleaned_data, user_food_pref, username)
 
-	# if C:
-		# search_by_truck_name()
-	if user_menu_choice == "X":
-		pass
-		# break
+		if user_menu_choice == "B":
+			print day_choices
+			user_day_pref = int(raw_input("Select your choice between 1-7. "))
+			search_by_day(cleaned_data, username, user_day_pref)
 
+		if user_menu_choice == "C":
+			user_truck_pref = str(raw_input("Where do you want to eat?"))
+			search_by_truck(cleaned_data, username, user_truck_pref)
 
+		elif user_menu_choice == "X":
+			break
 
 
 if __name__ == '__main__':
